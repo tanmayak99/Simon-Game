133ea136fb8d0c514dc1556088f4848a88117fdb
tanmayak99 <34204513+tanmayak99@users.noreply.github.com>
2022-01-19 22:51:12 +0530

    Add files via upload
    Adding HTML, CSS and JS.

---
+++ game.js
@@ -0,0 +1,81 @@
+
+var gameLevel = 0;
+var isGameOver = false;
+var gameStarted = false;
+
+var userClickedPattern = [];
+var gamePattern = [];
+const buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+
+  var randomNumber = Math.floor(Math.random()*4);
+
+  var randomChosenColour = buttonColours[randomNumber];
+
+  playSoundAndAnimate(randomChosenColour);
+
+  $("h1").text("Level " + ++gameLevel);
+
+  gamePattern.push(randomChosenColour);
+}
+
+function checkAnswer(userChosenColour) {
+
+  if(userChosenColour !== gamePattern[userClickedPattern.length-1]) {
+      playSoundAndAnimate("wrong");
+      gameOver();
+  }
+
+  else{
+    playSoundAndAnimate(userChosenColour);
+  }
+
+  if(userClickedPattern.length === gameLevel && isGameOver !== true){
+    console.log("New Level");
+    userClickedPattern = [];
+    setTimeout(function() {nextSequence();},500);
+  }
+}
+
+function playSoundAndAnimate(colour){
+
+  var audio = new Audio("sounds/" + colour + ".mp3");
+  audio.play();
+
+  if(colour !== "wrong")
+  $("#" + colour).fadeOut(100).fadeIn(100);
+}
+
+function gameOver() {
+
+  $("h1").text("Game Over. Press A key to start again.");
+
+  gameLevel = 0;
+  isGameOver = true;
+  gameStarted = false;
+
+  userClickedPattern = [];
+  gamePattern = [];
+}
+
+
+// Starting the game
+
+$(document).keypress(function () {
+   if(gameStarted === false) {
+     gameStarted = true;
+     nextSequence();
+   }
+});
+
+//
+
+$(".btn").click(function (){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    checkAnswer(userChosenColour);
+
+    console.log(userClickedPattern);
+});
